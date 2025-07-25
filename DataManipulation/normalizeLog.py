3062f5ff5da65e81c0c1b54fc8e5c4437b958f50
ryanbrittes <ryanbrittes027@gmail.com>
2025-07-25 18:57:42 -0400

    update

--- DataManipulation/normalizeLog.py
+++ DataManipulation/normalizeLog.py
@@ -2,3 +2,15 @@ import pandas as pd
 import numpy as np
 
 dataset = pd.read_csv('/home/ryan/Documents/Python/AI/AI_Training_Models/files/diabetes.csv')
+
+xValue = dataset[['Pregnancies']].values
+yValue = dataset[['Age']].values
+
+def logNormalizeFunction(rawValue):
+    listValue = []
+    const_not_zero = 1e-8
+    for i in range(len(rawValue)):
+        listValue.append(np.log(rawValue[i] + const_not_zero))
+    print(np.vstack(listValue))
+
+logNormalizeFunction(xValue)
--- DataManipulation/normalizeScoreZ.py
+++ DataManipulation/normalizeScoreZ.py
@@ -1,4 +1,36 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 dataset = pd.read_csv('/home/ryan/Documents/Python/AI/AI_Training_Models/files/diabetes.csv')
+
+xValue = dataset[['Pregnancies']].values
+yValue = dataset[['Age']].values
+
+def meanFunction(rawValue):
+    sumValue = 0
+    for i in range(len(rawValue)):
+        sumValue += rawValue[i]
+
+    return sumValue/len(rawValue)
+
+def standardDeviationFunction(rawValue):
+    mean = meanFunction(rawValue)
+    sumValue = 0
+    for i in range(len(rawValue)):
+        sumValue += (rawValue[i] - mean) ** 2
+    
+    return (sumValue / len(rawValue)) ** 0.5
+
+def scoreZFunction(rawValue):
+    meanValue = meanFunction(rawValue)
+    stdValue = standardDeviationFunction(rawValue)
+    listValue = []
+
+    for i in range(len(rawValue)):
+        value = (rawValue[i] - meanValue) / stdValue
+        listValue.append(value)
+    
+    return np.vstack(listValue)
+
+
